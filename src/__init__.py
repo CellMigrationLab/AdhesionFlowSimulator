f01b3609378f3a40fd2037695d5e6322e50af4d4
guijacquemet <guillaume.jacquemet@utu.fi>
2024-09-12 14:15:03 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,39 @@
+# __init__.py
+
+from .cell_dynamics import calculate_attachment_probability, calculate_cells_per_step, introduce_new_cells, check_overlap, sample_cell_surface
+from .flow_utils import calculate_flow_rate_per_frame, get_flow_vector, update_flow_field_with_phi_jax
+from .io_utils import load_receptor_map, get_masks_or_uniform, create_simulation_keys, generate_filename, save_parameters, create_video_from_positions, load_masks_and_compute_average
+from .simulations import Space, run_simulation
+
+# Metadata
+__version__ = "0.1.0"
+__author__ = "Guillaume Jacquemet"
+__email__ = "guillaume.jacquemet@abo.fi"
+
+# Package level imports
+__all__ = [
+    # cell_dynamics
+    "calculate_attachment_probability",
+    "calculate_cells_per_step",
+    "introduce_new_cells",
+    "check_overlap",
+    "sample_cell_surface",
+    
+    # flow_utils
+    "calculate_flow_rate_per_frame",
+    "get_flow_vector",
+    "update_flow_field_with_phi_jax",
+    
+    # io_utils
+    "load_receptor_map",
+    "get_masks_or_uniform",
+    "create_simulation_keys",
+    "generate_filename",
+    "save_parameters",
+    "create_video_from_positions",
+    "load_masks_and_compute_average",
+    
+    # simulations
+    "Space",
+    "run_simulation"
+]
